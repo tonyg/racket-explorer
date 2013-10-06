89ac46e9902b82a1c5737953dbaa824226137172
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2013-10-06 13:13:32 -0400

    Split out example

---
+++ example.rkt
@@ -0,0 +1,35 @@
+#lang racket/gui
+
+(require "main.rkt")
+
+(module+ main
+  (define e (explore (explorer-item "Hello, world" '(1 2 3) 'foo)))
+
+  (send e add-item!
+	(set "hello" 1 2 3.45 (lambda (x) x)))
+
+  (send e add-item!
+	(lambda (x #:y y #:z [z 2]) y))
+
+  (send e add-item!
+	(new (class object%
+	       (super-new)
+	       (field [x 5] [y 10]))))
+
+  (send e add-item! e)
+
+  (send e add-item!
+	(hash "hello" 1
+	      "world" 2))
+
+  (send e add-item!
+	(with-input-from-file "main.rkt"
+	  (lambda ()
+	    (read-line)
+	    (port->list))))
+
+  (struct x ()
+	  #:methods gen:explorable
+	  [(define (->explorer-item x)
+	     (explorer-item "Hi" '() 'bar))])
+  (send e add-item! (x)))
