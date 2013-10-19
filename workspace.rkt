960a53f78903ec5a2c80d288c56830595c039fee
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2013-10-19 01:13:18 +0100

    Clarify licensing

--- workspace.rkt
+++ workspace.rkt
@@ -2,6 +2,17 @@
 ;; A Smalltalk-like embeddable workspace.
 ;; Extracted and generalized from mred/private/repl.rkt.
 
+;; Copyright (c) 2010-2013 PLT Design Inc.
+;; Modifications copyright (c) 2013 Tony Garnock-Jones
+;;
+;; This module is distributed under the GNU Lesser General Public
+;; License (LGPL).  This means that you can link it into proprietary
+;; applications, provided you follow the rules stated in the LGPL.  You
+;; can also modify this module; if you distribute a modified version,
+;; you must distribute it under the terms of the LGPL, which in
+;; particular means that you must release the source code for the
+;; modified software.  See http://www.gnu.org/licenses/lgpl-3.0.txt for more information.
+
 (provide workspace%)
 
 (define repl-text%
