47d147b64c87eb39191759dec25a2a03fa6d9702
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-05-23 10:36:26 -0400

    Make it into a multi-collect package

---
+++ explorer/info.rkt
@@ -0,0 +1,5 @@
+#lang setup/infotab
+
+(define name "explorer")
+(define homepage "https://github.com/tonyg/racket-explorer")
+(define deps '("base" "gui-lib"))
--- info.rkt
+++ info.rkt
@@ -1,5 +1,2 @@
 #lang setup/infotab
-
-(define name "explorer")
-(define homepage "https://github.com/tonyg/racket-explorer")
-(define deps '("base" "gui-lib"))
+(define collection 'multi)
